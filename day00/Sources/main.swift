70484c2e3d2eef6f8caf2b08618b8d0eb23b88ac
Uttie Gumbula <uttieg@gmail.com>
2024-12-02 03:07:47 -0800

    finish it

--- day00/Sources/main.swift
+++ day00/Sources/main.swift
@@ -3,7 +3,7 @@
 
 import Foundation 
 
-let data = "/Users/uttie/Projects/advent_of_code/1/data"
+let data = "/Users/uttie/Projects/advent_of_code/day00/data"
 
 struct ListReader {
     private(set) var lhs: [Int] = []
@@ -14,22 +14,52 @@ struct ListReader {
         rhs.append(rhsValue)
     }
 
-    mutating func main() throws {
-        print("main world")
+    // parseData read a given filename and unmarshalls them into lhs, and rhs arrays respectively
+    mutating func parseData(filename: String) throws {
         
-        if let contents = try? String(contentsOfFile: data) {
-            let lines = contents.components(separatedBy: "\n")
+        // slurp data
+        if let contents = try? String(contentsOfFile: filename) {
+            let lines = contents.split(separator:"\n")
             for line in lines {
-                let values = line.split(separator: " ").map {Int($0)}
-                // TODO: make better
+                let values = line.split(separator: " ").compactMap {Int($0)}
+
+                // catch odd cases where parsing failed
                 if values.count != 2 {
+                    print(values)
                     throw ListReaderError.failedToUnpackData
                 }
-                addToList(values[0] as Int, values[1]? as Int)
+                addToList(values[0], values[1])
             }
         } else {
             throw ListReaderError.failedToReadData
         }
+    }
+
+    func part1() {
+        // how distance is calculated
+        func distance(i: Int, j: Int) -> Int {
+            return abs(i - j)
+        }
+
+        let l = lhs.sorted(by: <)
+        let r = rhs.sorted(by: <)
+
+        let answer = zip(l, r).compactMap(distance).sum()
+        print("Total answer: \(answer)")
+    }
+
+    func part2(){
+        // how distance is calculated
+        func distance(needle: Int) -> Int {
+            return needle * rhs.filter{$0 == needle}.count
+        }
+
+        let answer = lhs.map(distance).sum()
+        print("Total answer: \(answer)")
+    }
+
+    init(filename: String) throws {
+        try parseData(filename: filename)
     }
 }
 
@@ -49,4 +79,12 @@ extension ListReader {
     }
 }
 
-try ListReader().main()
+extension Sequence where Element: Numeric {
+    func sum() -> Element {
+        return reduce(0, +)
+    }
+}
+
+var l = try ListReader(filename: data)
+l.part1()
+l.part2()
